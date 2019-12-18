7adfe58627d8a035b9d1a9b293576a6266cca794
Link-yu <kevinyulk@163.com>
2019-12-18 23:55:03 +0800

    【netty测试】

--- src/main/java/com/weisi/nio/NIOServer.java
+++ src/main/java/com/weisi/nio/NIOServer.java
@@ -17,34 +17,41 @@ import java.util.Set;
  */
 public class NIOServer {
     public static void main(String[] args) throws Exception{
+        //创建serversocketchannel
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //创建Selector，静态方法创建
         Selector selector = Selector.open();
-
+        //serversocketchannel 绑定6666端口监听
         serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+        //serversocketchannel 设置非阻塞
         serverSocketChannel.configureBlocking(false);
-
+        //将serversocketchannel注册到selector
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
 
         while (true) {
+            //返回selectKeys（准备好的i/o，socketchannel），等待一秒
             if (selector.select(1000) == 0) {
                 System.out.println("服务器等待了1秒，无连接");
                 continue;
             }
-
+            //说明有返回selectkey
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
-
+            //遍历
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
 
             while(iterator.hasNext()) {
                 SelectionKey key = iterator.next();
+                //如果事件是OP_ACCEPT
                 if (key.isAcceptable()) {
+                    //拿到socketchannel
                     SocketChannel socketChannel = serverSocketChannel.accept();
                     System.out.println("客户端连接成功，生产了一个scoketChannel" + socketChannel.hashCode());
 
+                    //socketchanne设置非阻塞
                     socketChannel.configureBlocking(false);
+                    //设置监听READ事件
                     socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
-
                 }
 
                 if (key.isReadable()) {
